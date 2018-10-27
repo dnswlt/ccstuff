abcf7ff2c88b9159d3f7175814737ab56cce2065
UE69053 <dennis.walter@sbb.ch>
2018-10-27 11:30:44 +0200

    It worketh!

--- sudoku.cc
+++ sudoku.cc
@@ -14,7 +14,7 @@ public:
   int c;
   int num;
   std::bitset<9*9*9> mask;
-  Move(int r, int c, int num) : r(r), c(c), num(num) {
+  Move(int r, int c, int num) : r(r), c(c), num(num), mask{} {
     for (int i=0; i<9; i++) {
       mask[Ind(r, i, num)] = true;
       mask[Ind(i, c, num)] = true;
@@ -23,47 +23,51 @@ public:
     int bc = c / 3;
     for (int i=0; i<3; i++) {
       for (int j=0; j<3; j++) {
-        mask[Ind(br+i, bc+j, num)] = true;
+        mask[Ind(br*3+i, bc*3+j, num)] = true;
       }
     }
   }
 };
 
 class State {
-public:
   std::bitset<9*9*9> mask;
-  State(const Move *move) : mask{} {
-    mask |= move->mask;
+  const Move *move_;
+public:
+  State(const Move *move) : move_(move), mask(move->mask) {
   }
-  State(const State& pred, const Move *move) {
-    mask = pred.mask | move->mask;
+  State(const State& pred, const Move *move) : move_(move), mask(pred.mask | move->mask) {
   } 
+  bool Possible(int r, int c, int num) const {
+    return !mask.test(Ind(r, c, num));
+  }
+  const Move *move() const { return move_; }
 };
 
 class Board {
 public:
-  std::vector<const Move*> moves;
+  int backtrack_cnt = 0;
+  std::vector<const Move*> all_moves;
   std::vector<State> states;
-  std::vector<const Move*> made_moves;
-  Board() : moves(9*9*9) {
+  int nums[9][9];
+  Board() : all_moves(9*9*9), nums{} {
     int idx = 0;
     for (int i=0; i<9; i++) {
       for (int j=0; j<9; j++) {
         for (int n=1; n<=9; n++) {
-          moves[idx] = new Move(i, j, n);
+          all_moves[idx] = new Move(i, j, n);
           idx++;
         }
       }
     }
   }
   ~Board() {
-    for (auto const m : moves) {
+    for (auto const m : all_moves) {
       delete m;
     }
     std::cout << "Board deleted\n";
   }
   void MakeMove(const Move *m) {
-    made_moves.push_back(m);
+    nums[m->r][m->c] = m->num;
     if (states.size() == 0) {
       states.push_back(State(m));
     } else {
@@ -71,11 +75,13 @@ public:
     }
   }
   void Backtrack() {
+    auto const m = Current().move();
     states.pop_back();
-    made_moves.pop_back();
+    nums[m->r][m->c] = 0;
+    backtrack_cnt++;
   }
   const Move* GetMove(int r, int c, int num) {
-    return moves[r * 81 + c * 9 + num];
+    return all_moves[Ind(r, c, num)];
   }
   bool Done() {
     return states.size() == 81;
@@ -83,17 +89,40 @@ public:
   const State& Current() {
     return states[states.size()-1];
   }
-  int EvalMove(const Move *m) {
-    if (Current().mask[Ind(m->r, m->c, m->num)]) {
-      return -1;
+  std::vector<const Move*> NextMoves() {
+    if (states.size() == 0) {
+      return std::vector<const Move*>({all_moves[0]});
     }
-    int choices = 0;
-    for (int n=1; n<=9; n++) {
-      if (!Current().mask[Ind(m->r, m->c, n)]) {
-        choices++;
+    int ps[9][9] = {};
+    for (auto const m : all_moves) {
+      if (nums[m->r][m->c] == 0 && Current().Possible(m->r, m->c, m->num)) {
+        ps[m->r][m->c]++;
       }
     }
-    return choices;
+    int min_i = -1;
+    int min_j = 0;
+    for (int i=0; i<9; i++) {
+      for (int j=0; j<9; j++) {
+        if (nums[i][j] > 0) {
+          // already assigned
+          continue;
+        } else if (ps[i][j] == 0) {
+          // no possible choices: time to backtrack
+          return std::vector<const Move*>();  
+        } else if (min_i == -1 || ps[i][j] < ps[min_i][min_j]) {
+          min_i = i;
+          min_j = j;
+        }
+      }
+    }
+    std::vector<const Move*> result;
+    for (int n=1; n<=9; n++) {
+      const Move *m = GetMove(min_i, min_j, n);
+      if (Current().Possible(m->r, m->c, m->num)) {
+        result.push_back(m);
+      }
+    }
+    return result;
   }
 };
 
@@ -102,38 +131,19 @@ bool Solve(Board& board) {
   if (board.Done()) {
     return true;
   }
-  const State& s = board.Current();
-  std::vector<const Move*> next_moves;
-  std::vector<int> values;
-  for (auto const m : board.moves) {
-    int v = board.EvalMove(m);
-    if (v > 0) {
-      next_moves.push_back(m);
-      values.push_back(v);
+  for (auto const m : board.NextMoves()) {
+    board.MakeMove(m);
+    bool solved = Solve(board);
+    if (solved) {
+      return true;
     }
-  }
-  int value = 1;
-  while (value <= 9) {
-    for (int i=0; i<next_moves.size(); i++) { 
-      if (values[i] == value) {
-        board.MakeMove(next_moves[i]);
-        bool solved = Solve(board);
-        if (solved) {
-          return true;
-        }
-        board.Backtrack();
-      }
-    }
-    ++value;
+    board.Backtrack();
   }
   return false;
 }
 
 std::ostream &operator<<(std::ostream& os, const Board& board) {
-  int b[9][9] = {};
-  for (auto const m : board.made_moves) {
-    b[m->r][m->c] = m->num;
-  }
+  auto b = board.nums;
   for (int i=0; i<9; i++) {
     for (int j=0; j<9; j++) {
       if (b[i][j] > 0) {
@@ -175,11 +185,11 @@ int main(int argc, char **argv) {
     }
     row++;
   } 
-  std::cout << board.states.size() << " moves read\n";
+  std::cout << "Board with " << board.states.size() << " numbers read\n";
   std::cout << board << "\n";
   bool solved = Solve(board);
   if (solved) {
-    std::cout << "Board solved\n";
+    std::cout << "Board solved, backtracked " << board.backtrack_cnt << " times\n";
     std::cout << board;
   } else {
     std::cout << "Board unsolved\n";
