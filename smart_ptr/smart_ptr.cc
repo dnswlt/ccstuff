8e87667892f43380e06c8c8498fed9c72ff000b1
Dennis Walter <dennis.walter@gmail.com>
2018-12-17 20:28:40 +0100

    Added comments

--- smart_ptr/smart_ptr.cc
+++ smart_ptr/smart_ptr.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 
+// Widget is a simple class used for experiments with smart pointers
 class Widget {
 private:
   int x_;
@@ -10,6 +11,7 @@ public:
   ~Widget();
 };
 
+// Save some letters
 using UPWidget = std::unique_ptr<Widget>;
 
 int Widget::x() {
@@ -30,8 +32,9 @@ void ProcessWidget(UPWidget w) {
 
 int main() {
   auto w1 = std::make_unique<Widget>(3);
-  // auto w2 = std::move(w1);
+  // Pass w1 to function, transferring ownership
   ProcessWidget(std::move(w1));
+  // Expect that w1 was set to NULL due to transfer
   if (w1 != nullptr) {
     std::cout << "Expected w1 to be set to nullptr\n";
     return 1;
